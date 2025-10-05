256f42512d906b6bb07ba1732cbb99e36998bec4
Arbaz Khan <ak2208803@gmail.com>
2025-10-05 11:29:04 +0530

    the navbar convert into responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -112,6 +112,79 @@ const Navbar = () => {
             Create account
           </button>
         )}
+
+        <img
+          onClick={() => setShowMenu(true)}
+          className=""
+          src={assets.menu_icon}
+          alt=""
+        />
+        {/* mobile menu */}
+
+        <div
+          className={`${
+            showMenu ? "fixed w-full" : "hidden"
+          } md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+          <div className="flex items-center justify-between px-5 py-6">
+            <img className="w-36" src={assets.logo} alt="" />
+            <img
+              className="w-7"
+              onClick={() => setShowMenu(false)}
+              src={assets.cross_icon}
+              alt=""
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink
+              to="/"
+              onClick={() => setShowMenu(false)}
+              className={({ isActive }) =>
+                `px-4 py-2 rounded inline-block transition-colors duration-300 ${
+                  isActive ? "bg-[#5f6fff] text-white" : "hover:bg-gray-100"
+                }`
+              }
+            >
+              HOME
+            </NavLink>
+
+            <NavLink
+              to="/doctors"
+              onClick={() => setShowMenu(false)}
+              className={({ isActive }) =>
+                `px-4 py-2 rounded inline-block transition-colors duration-300 ${
+                  isActive ? "bg-[#5f6fff] text-white" : "hover:bg-gray-100"
+                }`
+              }
+            >
+              ALL DOCTORS
+            </NavLink>
+
+            <NavLink
+              to="/about"
+              onClick={() => setShowMenu(false)}
+              className={({ isActive }) =>
+                `px-4 py-2 rounded inline-block transition-colors duration-300 ${
+                  isActive ? "bg-[#5f6fff] text-white" : "hover:bg-gray-100"
+                }`
+              }
+            >
+              ABOUT
+            </NavLink>
+
+            <NavLink
+              to="/contact"
+              onClick={() => setShowMenu(false)}
+              className={({ isActive }) =>
+                `px-4 py-2 rounded inline-block transition-colors duration-300 ${
+                  isActive ? "bg-[#5f6fff] text-white" : "hover:bg-gray-100"
+                }`
+              }
+            >
+              CONTACT
+            </NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
