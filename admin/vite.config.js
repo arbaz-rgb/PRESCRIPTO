c5d5e466f885e9833fb1dde95bdeb604814f7092
Arbaz Khan <ak2208803@gmail.com>
2025-10-12 12:32:52 +0530

    the add doctor page completer

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import scrollbarHide from "tailwind-scrollbar-hide";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss({
+      plugins: [scrollbarHide], // ✅ this is the correct modern syntax
+    }),
+  ],
   server: { port: 5174 },
 });
